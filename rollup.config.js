8e250e30e50333816264db47e9e4b71456f93d29
Sergey Maksimenko <serega.maksimenko@gmail.com>
2022-11-15 14:08:43 +0600

    updated configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -21,14 +24,22 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
+      postcss({
+        extract: false,
+        modules: true,
+      }),
+
+      terser(),
     ],
   },
   {
     input: 'dist/esm/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
+    external: [/\.(sc|sa|c)ss$/],
   },
 ];
